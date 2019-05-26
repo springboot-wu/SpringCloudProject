545d34f98fa5a47230e00d3184be715c2d35d117
whj <1563866551@qq.com>
2019-05-26 18:17:10 +0800

    111 222

--- src/main/java/org/wu/controller/MyTestController.java
+++ src/main/java/org/wu/controller/MyTestController.java
@@ -4,9 +4,9 @@ public class MyTestController {
 
 	public static void main(String[] args) {
 		
-		System.out.println("我是分支数据");
+		System.out.println("111");
 		
-		System.out.println("我是分支数据");
+		System.out.println("22");
 		//gaosuwu
 
 	}
